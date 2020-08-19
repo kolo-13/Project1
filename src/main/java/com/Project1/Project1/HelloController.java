0fb9de686e624e55c2ca454fb1f49148aab7e699
kolo-13 <e.mkolarik@gmail.com>
2020-08-19 19:48:02 +0200

    controller2

---
+++ src/main/java/com/Project1/Project1/HelloController.java
@@ -0,0 +1,14 @@
+package com.Project1.Project1;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+
+	@RequestMapping("/")
+	public String index() {
+		return "Greetings from Spring Boot!";
+	}
+
+}
